49b8e9c9f1b28c1ce3570e495f1ce924d862a619
Shane BRIGGS <sbriggs@c4r12s6.wethinkcode.co.za>
2019-12-12 16:21:51 +0200

    stopping threads

--- broker-artifact-id/src/main/java/brokergroupid/Main.java
+++ broker-artifact-id/src/main/java/brokergroupid/Main.java
@@ -3,24 +3,27 @@ import java.net.*; //Socket
 import java.io.*; //Exception
 import java.util.Scanner;
 public class Main
-{   
+{
     public static Scanner scanner;
     public static void main(String[] args){
         System.out.println( "Broker: Hello World!" );
         try{
-            Socket s = new Socket("localhost", 5000); //connects to the specified port number on the named host
-            OutputStream os = s.getOutputStream();
-            PrintWriter pw = new PrintWriter(os);
+            Socket socket = new Socket("localhost", 5000);
+/*             OutputStream os = s.getOutputStream();
+            PrintWriter pw = new PrintWriter(os); */
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
             scanner = new Scanner(System.in);
             while(scanner.hasNext()){
-                String brokerStr = scanner.nextLine();
-                pw.println(brokerStr);
-                pw.flush();
-                InputStream is = s.getInputStream();
+                String outString = scanner.nextLine();
+                printWriter.println(outString);
+                printWriter.flush();
+/*                 InputStream is = s.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String str = br.readLine();
-                System.out.println("server: " + str);
+                BufferedReader br = new BufferedReader(isr); */
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String inputStreamLine = bufferedReader.readLine();
+                //System.out.println("Server: " + inputStreamLine);
+                System.out.println("\nMessage from:\nServer\nContent:\n" + inputStreamLine + "\n");
             }
         } catch (IOException e){
         System.err.println(e.getMessage());
--- router-artifact-id/src/main/java/routergroupid/Main.java
+++ router-artifact-id/src/main/java/routergroupid/Main.java
@@ -1,52 +1,70 @@
 package routergroupid;
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Router: Hello World!" );
+import java.nio.channels.*;
+public class Main{
+    public static void main( String[] args ){
         try{
-          
-            ServerSocket brokerListener = new ServerSocket(5000); //there is some kind of weird automatic loop thing going on over here
-            Socket brokerSocket = brokerListener.accept();
-            System.out.println( "broker client connected:" +
-                                "\n ListenerLocalPort" + brokerListener.getLocalPort() +
-                                "\n ListenerInetAddress" + brokerListener.getInetAddress() +
-                                "\n SocketLocalPort" + brokerSocket.getLocalPort() +
-                                "\n SocketInetAddress" + brokerSocket.getInetAddress() +
-                                "\n SocketPort" + brokerSocket.getPort());
-
-            Thread thread = new Thread(new ClientRunnable(brokerSocket));
-            thread.start();
-            //thread.join();
-           
-            ServerSocket marketListener = new ServerSocket(5001);
-            Socket marketSocket = marketListener.accept();
-            System.out.println("market client connected");
-            new Thread(new ClientRunnable(marketSocket)).start();
-
-        } catch (IOException e){
-        System.err.println(e.getMessage());
+            Thread brokerThread = new Thread(new ListenerRunnable(new ServerSocket(5000)));
+            brokerThread.start();
+            Thread marketThread = new Thread(new ListenerRunnable(new ServerSocket(5001)));
+            marketThread.start();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
 
-final class ClientRunnable implements Runnable{
+class ListenerRunnable implements Runnable{
+    
+    ServerSocket serverSocket;
+
+    ListenerRunnable(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+        System.out.println("Listener started on: " + serverSocket.getLocalPort());
+    }
+
+    @Override
+    public void run() {
+        try{
+            while(true){        
+                Socket socket = serverSocket.accept();
+                Thread thread = new Thread(new ClientRunnable(socket));
+                thread.start();
+            }
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
+class ClientRunnable implements Runnable{
     private Socket socket;
+    private String outString;
+    //private String uniqueID;
     ClientRunnable(Socket socket){
         this.socket = socket;
+        //this.uniqueID = socket.toString();
     }
     @Override
     public void run(){
+        System.out.println("\nClient connected:\n" + socket.toString() + "\n");
         try{
             while(true){
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String inputStreamLine = bufferedReader.readLine();
-                System.out.println("client: " + inputStreamLine);
+                if(inputStreamLine == null){
+                    System.out.println("\nClient disconnected:\n" + socket.toString() + "\n");
+                    break;
+                }
+                System.out.println("\nMessage from: \n" + socket.toString() + "\nContent:\n" + inputStreamLine + "\n");
+                if(inputStreamLine.equals("hello")){
+                    outString = "hello to you too";
+                } else {
+                    outString = "say what?";
+                }
                 PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                printWriter.println("I am a router");
+                printWriter.println(outString);
                 printWriter.flush();
             }
         } catch (IOException e){
@@ -54,3 +72,24 @@ final class ClientRunnable implements Runnable{
         }
     }
 }
+
+/* 
+
+System.out.println("Listener started on: \n" + 
+serverSocket.getLocalPort() + "\n" +
+serverSocket.getInetAddress() + "\n" +
+serverSocket.getLocalSocketAddress() + "\n");
+
+System.out.println("Client connected: \n" +
+socket.getInetAddress() + "\n" +
+socket.getLocalAddress() + "\n" +
+socket.getLocalPort() + "\n" +
+socket.getLocalSocketAddress() + "\n" +
+socket.getPort() + "\n" +
+socket.getRemoteSocketAddress() + "\n" +
+socket.isBound() + "\n" +
+socket.isClosed() + "\n" +
+socket.isConnected() + "\n" +
+socket.isInputShutdown() + "\n" +
+socket.isOutputShutdown());
+ */
